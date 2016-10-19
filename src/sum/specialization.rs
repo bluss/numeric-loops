68f0e847ced607deb60e252562f8ed3e87715dcf
bluss <bluss@users.noreply.github.com>
2016-10-19 21:52:15 +0200

    Add specializations for integers

--- src/sum/specialization.rs
+++ src/sum/specialization.rs
@@ -29,5 +29,29 @@ macro_rules! float_impl {
     }
 }
 
+macro_rules! int_impl {
+    ($($f:ty)*) => {
+        $(
+        impl Sum for $f {
+            fn sum(lhs: &[Self]) -> Self {
+                lhs.iter()
+                   .fold(0, |acc, &z| acc + z)
+            }
+        }
+
+        impl Dot for $f {
+            fn dot(xs: &[Self], ys: &[Self]) -> Self {
+                xs.iter()
+                  .zip(ys)
+                  .map(|(&x, &y)| x + y)
+                  .fold(0, |acc, z| acc + z)
+            }
+        }
+        )*
+    }
+}
+
 float_impl!(f32);
 float_impl!(f64);
+
+int_impl!(u8 i8 u16 i16 u32 i32 u64 i64 usize isize);
